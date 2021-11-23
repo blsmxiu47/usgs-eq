3d179e1a545a1baa90d767b8fc47b34240cbd42f
Wes Warriner <fridgehistory@gmail.com>
2021-11-22 16:50:36 -0800

    update usgs pytests with vcrpy caching and add earthquakes_get_summary test

--- tests/test_usgs.py
+++ tests/test_usgs.py
@@ -1,13 +1,35 @@
 import sys
 # print(sys.path)
 sys.path.append('C:\\Users\\weswa\\Projects\\py-natural-events\\earthquakes\\usgs')
+
+from pytest import fixture
+import vcr
 from usgs import Earthquakes
 
-def test_earthquakes_get_summary():
+
+@fixture
+def summary_keys():
+    return ['type', 'metadata', 'features', 'bbox']
+
+
+@vcr.use_cassette('tests/vcr_cassettes/eq-get-summary.yaml')
+def test_earthquakes_get_summary(summary_keys):
     """Tests USGS Earthquakes GeoJSON Feed API call to get FeatureCollection metadata"""
 
     eq_instance = Earthquakes()
-    response = eq_instance.get_summary(start="2021-10-01", end="2021-10-31")
+    response = eq_instance.get_summary()
+
+    assert isinstance(response, dict), "Response should be of type 'dict'"
+    assert response['type'] == 'FeatureCollection', "Response 'type' should be 'FeatureCollection'"
+    assert set(summary_keys).issubset(response.keys()), "All keys should be in the response"
+
+
+@vcr.use_cassette('tests/vcr_cassettes/eq-get-geojson.yaml')
+def test_earthquakes_get_geojson():
+    """Tests USGS Earthquakes GeoJSON Feed API call to get FeatureCollection metadata"""
+
+    eq_instance = Earthquakes()
+    response = eq_instance.get_geojson(start='2021-10-01', end='2021-10-03')
 
     assert isinstance(response, dict)
     assert response['type'] == 'FeatureCollection'
