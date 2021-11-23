914cf88b7a23c55d2ab1c0fb6f3fd3dd19969031
Wes Warriner <fridgehistory@gmail.com>
2021-11-22 16:54:56 -0800

    update usgs Earthquakes class with methods to actually make requests of USGS's API using requests_cache sessions

--- usgs/earthquakes.py
+++ usgs/earthquakes.py
@@ -1,6 +1,22 @@
+from . import session
+
 class Earthquakes(object):
     def __init__(self) -> None:
-        super().__init__()
-    
-    def get_summary(self, start="2021-01-01", end="2021-01-02"):
-        return {'type': 'FeatureCollection'}
+        super().__init__()    
+
+
+    def get_summary(self, timeframe='hour', min_magnitude=None):
+        if min_magnitude is None:
+            min_magnitude = 'all'
+        else:
+            min_magnitude = str(min_magnitude)
+            assert min_magnitude in ['1.0', '2.5', '4.5', 'significant']
+        path = f'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/{min_magnitude}_{timeframe}.geojson'
+        response = session.get(path)
+        return response.json()
+
+
+    def get_geojson(self, start='2021-01-01', end='2021-01-02'):
+        path = f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start}&endtime={end}'
+        response = session.get(path)
+        return response.json()
